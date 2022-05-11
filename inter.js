f89d3baef130bd85dd8416770a46b909216cf5b3
wesley oliveira <96589369+wos13b@users.noreply.github.com>
2022-05-11 13:41:49 -0300

    a

--- inter.js
+++ inter.js
@@ -1,8 +1,21 @@
 conteudo = document.getElementById('conteudo')
+logo = document.getElementById('logo')
 conteudo.addEventListener('click', conteudo_click)
+conteudo.addEventListener('mouseenter', conteudo_enter)
+conteudo.addEventListener('mouseout', conteudo_exit)
 
 function conteudo_click(){
 
     conteudo.classList.toggle('active')
+
+}
+function conteudo_enter(){
+
+    conteudo.style.background = '#3f3f3f'
+
+}
+function conteudo_exit(){
+
+    conteudo.style.background = 'black'
 
 }
